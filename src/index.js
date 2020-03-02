46e99b9dcfa6ecdc5846e467b6a8c6b11efef9e1
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-03-03 00:15:45 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let binLetter = expr;
+    let binLetter10 = "";
+    let binLetter2 = "";
+    let OneLetterMorse = "";
+    let decodeLetter = "";
+    
+    while (binLetter.length !== 0) {
+        binLetter10 = binLetter.substr(0, 10);
+
+        if (binLetter10 == "**********") {
+            decodeLetter += " ";
+        }   else {
+            while (binLetter10.length !== 0) {
+                binLetter2 = binLetter10.substr(0, 2);
+
+                if (binLetter2 == "10") {
+                    OneLetterMorse += ".";
+                }   else if (binLetter2 == "11") {
+                    OneLetterMorse += "-";
+                }   else {
+                    OneLetterMorse += "";
+                };
+
+                binLetter10 = binLetter10.substr(2);
+            };
+
+            decodeLetter += MORSE_TABLE[OneLetterMorse];
+        };
+
+        binLetter2 = "";
+        OneLetterMorse = "";
+        binLetter = binLetter.substr(10);
+    };
+
+    return decodeLetter;
 }
 
 module.exports = {
